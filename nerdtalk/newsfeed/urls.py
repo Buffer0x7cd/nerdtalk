928dd8030a3e0e7a49d0b26ddb9bf1e489fd593f
buffer0x7cd <manvendra021997@gmail.com>
2018-01-29 19:00:42 +0530

    Implemented redirects for authenticated and unauthenticated users

--- nerdtalk/newsfeed/urls.py
+++ nerdtalk/newsfeed/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from newsfeed import views
 urlpatterns = [
+    url(r'^$', views.root, name='root'),
     url(r'^home/', views.home, name="home"),
 ]
--- nerdtalk/newsfeed/views.py
+++ nerdtalk/newsfeed/views.py
@@ -1,6 +1,13 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import login
 @login_required
 def home(request):
     return render(request, 'newsfeed/home.html')
+
+
+def root(request):
+    if request.user.is_authenticated():
+        return redirect('home')
+    else:
+        return redirect('login')
